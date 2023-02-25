366069ff0c9b3b40998f9700755d777f57a20401
RAVI-VERMA-1 <ravi.verma.0814@gmail.com>
2023-02-25 21:26:45 +0530

    done

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -67,33 +67,7 @@ class RecipeView extends View {
       }</span>
       <span class="recipe__info-text">minutes</span>
     </div>
-    <div class="recipe__info">
-      <svg class="recipe__info-icon">
-        <use href="${icons}#icon-users"></use>
-      </svg>
-      <span class="recipe__info-data recipe__info-data--people">${
-        this._data.servings
-      }</span>
-      <span class="recipe__info-text">servings</span>
-
-      <div class="recipe__info-buttons">
-        <button class="btn--tiny btn--update-servings" data-update-to=${
-          this._data.servings - 1
-        }>
-          <svg>
-            <use href="${icons}#icon-minus-circle"></use>
-          </svg>
-        </button>
-        <button class="btn--tiny btn--update-servings" data-update-to=${
-          this._data.servings + 1
-        }>
-          <svg>
-            <use href="${icons}#icon-plus-circle"></use>
-          </svg>
-        </button>
-      </div>
-    </div>
-
+    
     <div class="recipe__user-generated ${this._data.key ? '' : 'hidden'}">
        <svg>
        <use href="${icons}#icon-user"></use>
@@ -101,7 +75,7 @@ class RecipeView extends View {
 
     </div>
     <button class="btn--round btn--bookmark">
-      <svg class="">
+      <svg>
         <use href="${icons}#icon-bookmark${
       this._data.bookmarked ? '-fill' : ''
     }"></use>
@@ -121,18 +95,15 @@ class RecipeView extends View {
   <div class="recipe__directions">
     <h2 class="heading--2">How to cook it</h2>
     <p class="recipe__directions-text">
-      This recipe was carefully designed and tested by
-      <span class="recipe__publisher">${
-        this._data.publisher
-      }</span>. Please check out
-      directions at their website.
+     Publisher of the recipe is:
+      <span class="recipe__publisher">${this._data.publisher}</span>.
     </p>
     <a
       class="btn--small recipe__btn"
       href="${this._data.source_url}"
       target="_blank"
     >
-      <span>Directions</span>
+      <span>RELOAD PAGE</span>
       <svg class="search__icon">
         <use href="${icons}#icon-arrow-right"></use>
       </svg>
